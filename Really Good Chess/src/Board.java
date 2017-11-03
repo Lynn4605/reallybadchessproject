066ca61d83f3f0f9f1814dcda5f27b34406954ad
Lynn4605 <33323427+Lynn4605@users.noreply.github.com>
2017-11-03 11:35:05 -0400

    Update Board.java

--- Really Good Chess/src/Board.java
+++ Really Good Chess/src/Board.java
@@ -1,1 +1,31 @@
+import java.util.Arrays;
 
+public class ChessBoard {
+
+   
+   //The size of the board.
+    public static final int SIZE = 8;
+
+  //piece colors
+    private static final boolean WHITE = true;
+    private static final boolean BLACK = false;
+
+private final Piece[][] UNRANDOMIZED = {
+            { new Rook(WHITE), new Knight(WHITE), new Bishop(WHITE),
+                    new Queen(WHITE), new King(WHITE), new Bishop(WHITE),
+                    new Knight(WHITE), new Rook(WHITE) },
+            { new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE),
+                    new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE),
+                    new Pawn(WHITE), new Pawn(WHITE) },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK),
+                    new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK),
+                    new Pawn(BLACK), new Pawn(BLACK) },
+            { new Rook(BLACK), new Knight(BLACK), new Bishop(BLACK),
+                    new Queen(BLACK), new King(BLACK), new Bishop(BLACK),
+                    new Knight(BLACK), new Rook(BLACK) } };
+  
+  
