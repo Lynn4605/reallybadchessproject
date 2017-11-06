f0986d9d988ee239e8f11abe1d187ef4be93236a
NABabar <33323437+NABabar@users.noreply.github.com>
2017-11-06 09:43:41 -0500

    Update King.java

--- Really Good Chess/src/King.java
+++ Really Good Chess/src/King.java
@@ -1,1 +1,41 @@
-
+public class king{
+  public king(int c, int x, int y )
+    if (c==1){
+    }
+  public void up(){
+    y+=100;
+    repaint;
+  }
+  public void down(){
+    y-=100;
+    repaint;
+  }
+  public void left(){
+    x-=100;
+    repaint;
+  }
+  public void right(){
+    x+=100;
+    repaint;
+  }
+  public void upright(){
+    x+=100;
+    y+=100;
+    repaint;
+  }
+  public void upleft(){
+    x-=100;
+    y+=100;
+    repaint;
+  }
+  public void downleft(){
+    x-=100;
+    y-=100;
+    repaint;
+  }
+  public void downright(){
+    x+=100;
+    y-=100;
+    repaint;
+  }
+}
