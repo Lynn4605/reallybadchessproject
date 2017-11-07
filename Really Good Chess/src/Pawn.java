9c53482b9c2bb16768d8c62fa49bae4802425c8d
Lynn4605 <33323427+Lynn4605@users.noreply.github.com>
2017-11-07 08:30:37 -0500

    Update Pawn.java

--- Really Good Chess/src/Pawn.java
+++ Really Good Chess/src/Pawn.java
@@ -7,3 +7,26 @@ public class Pawn{
     if (c==0){
     }
     
+    public void up{
+      if (y<700){
+        y+=100;
+      }
+      //convert to a dead piece
+      else{ System.out.println("Cannot move forward");
+          }
+    }
+    
+    public void upright{
+      //if (there's an enemy piece there){
+    //}
+      x+=100;
+      y+=100;
+    }
+    
+    public void upleft{
+  //if (there's an enemy piece there){
+    //}
+      x-=100;
+      y+=100;
+    }
+    
