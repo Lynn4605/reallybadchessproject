64c83ddd31b84b9265ae848fced4be20128bfa94
angele33 <33323362+angele33@users.noreply.github.com>
2017-11-07 08:46:47 -0500

    Update Knight.java

--- Really Good Chess/src/Knight.java
+++ Really Good Chess/src/Knight.java
@@ -1,49 +1,55 @@
 public class knight{
   public knight(int c, int x, int y )
-    //if the knight is white
+    //if the knight is black
     if (c==1){
     }
-  //if the knight is black
+  //if the knight is white
   if(c==0){
   }
   public void upRight(){
-    x+=100;
-      y+=300;
-    repaint;
-  }
-  public void upLeft(){
-    x-=100;
-    x+=300;
-    repaint;
-  }
-  public void downRight(){
+    if(x<700 || y>200){
     x+=100;
     y-=300;
+    repaint;
+    } else
+      System.out.println("Knight cannnot move up and right");
+  }
+  public void upLeft(){
+    if(x>100 || y>200){
+    x-=100;
+    y-=300;
+    repaint;
+    }
+  }
+  public void downRight(){
+    if(x
+    x+=100;
+    y+=300;
     repaint;
   }
   public void downLeft(){
     x-=100;
-    x-=300;
+    y+=300;
     repaint;
   }
   public void rightSideUp(){
     x+=300;
-    y+=100;
+    y-=100;
     repaint;
   }
   public void rightSideDown(){
     x+=300;
-    y-=100;
+    y+=100;
     repaint;
   }
   public void leftSideUp(){
     x-=300;
-    y+=100;
+    y-=100;
     repaint;
   }
   public void rightSideDown(){
     x-=300;
-    x-=100;
+    y+=100;
     repaint;
   }
 
