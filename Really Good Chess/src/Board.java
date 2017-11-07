edfd12937b92ac01ddff1a4c8d44826d36e87574
NABabar <33323437+NABabar@users.noreply.github.com>
2017-11-07 08:38:05 -0500

    Update Board.java

--- Really Good Chess/src/Board.java
+++ Really Good Chess/src/Board.java
@@ -2,53 +2,55 @@ import java.util.Arrays;
 import javax.swing.*;
 
 public class ChessBoard {
+//----------------------------------------------------
+   import javax.swing.JFrame;
 
+	import javax.swing.JPanel;
+	 
+	//Your extension may require you to modify this class.
+	//Otherwise, nothing in here needs to change.
+	public class Chessboard extends JFrame
+	{
+	                private AnimationPanel panel;
+	                
+	                public Chessboard()
+	                {
+                      private final Piece[] board = new Piece[64];
+                      private final Piece[] white = { new Rook(WHITE), new Knight(WHITE), new Bishop(WHITE),
+                      new Queen(WHITE), new King(WHITE), new Bishop(WHITE),
+                      new Knight(WHITE), new Rook(WHITE), new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE),
+                      new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE),
+                      new Pawn(WHITE), new Pawn(WHITE) };
+                      private final Piece[] black = { new Rook(BLACK), new Knight(BLACK), new Bishop(BLACK),
+                      new Queen(BLACK), new King(BLACK), new Bishop(BLACK),
+                      new Knight(BLACK), new Rook(BLACK), new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK),
+                      new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK),
+                      new Pawn(BLACK), new Pawn(BLACK) };
+                      int w=0;
+                      while (int w<16){
+                         board[w]=(white[((int)(Math.random()*16))]);
+                         w++;
+                      }
+                      int b=47;
+                      while (int b<64){
+                         board[b]=(black[((int)(Math.random()*16))]);
+                         b++;
+                      }
+                         
+	                   setSize(810, 840);
+	                   panel = new AnimationPanel();
+	                   add(panel);
+	                   this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	                   setVisible(true);
+	                   setResizable(false);
+	                   panel.requestFocus(true);
+	                }
+	                
+	                public static void main(String[]args)
+	                {
+	                                new Chessboard();
+	                }
+	}
+//-----------------------------------------------
    
-   //The size of the board. 8*8?
-    public static final int SIZE = 8;
 
-  //piece colors
-    private static final boolean WHITE = true;
-    private static final boolean BLACK = false;
-   
-   private JFrame gameframe = new JFrame("Really Good Chess");
-   gameframe.setSize(1000, 1000);
-   gameframe.setLayout(null);
-   gameframe.setResizable(false);
-   gameframe.setVisible(true);
-   
-   
-   private final Piece[] board = new Piece[64];
-//what the board would look like without any randomization 
-private final Piece[][] UNRANDOMIZED = {
-            { new Rook(WHITE), new Knight(WHITE), new Bishop(WHITE),
-                    new Queen(WHITE), new King(WHITE), new Bishop(WHITE),
-                    new Knight(WHITE), new Rook(WHITE) },
-            { new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE),
-                    new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE),
-                    new Pawn(WHITE), new Pawn(WHITE) },
-            { null, null, null, null, null, null, null, null },
-            { null, null, null, null, null, null, null, null },
-            { null, null, null, null, null, null, null, null },
-            { null, null, null, null, null, null, null, null },
-            { new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK),
-                    new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK),
-                    new Pawn(BLACK), new Pawn(BLACK) },
-            { new Rook(BLACK), new Knight(BLACK), new Bishop(BLACK),
-                    new Queen(BLACK), new King(BLACK), new Bishop(BLACK),
-                    new Knight(BLACK), new Rook(BLACK) } };
-   private final Piece[][] white ={
-      { { new Rook(WHITE), new Knight(WHITE), new Bishop(WHITE),
-                    new Queen(WHITE), new King(WHITE), new Bishop(WHITE),
-                    new Knight(WHITE), new Rook(WHITE) },
-            { new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE),
-                    new Pawn(WHITE), new Pawn(WHITE), new Pawn(WHITE),
-                    new Pawn(WHITE), new Pawn(WHITE) } };
-      private final Piece[][] black = {{ new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK),
-                    new Pawn(BLACK), new Pawn(BLACK), new Pawn(BLACK),
-                    new Pawn(BLACK), new Pawn(BLACK) },
-            { new Rook(BLACK), new Knight(BLACK), new Bishop(BLACK),
-                    new Queen(BLACK), new King(BLACK), new Bishop(BLACK),
-                    new Knight(BLACK), new Rook(BLACK) } };
-  
-  
