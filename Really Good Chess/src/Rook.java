def40a009c552c7812fff38f324a06debe3d36b3
Lynn4605 <33323427+Lynn4605@users.noreply.github.com>
2017-11-08 09:05:07 -0500

    Update Rook.java

--- Really Good Chess/src/Rook.java
+++ Really Good Chess/src/Rook.java
@@ -1,5 +1,11 @@
-Public Class Rook{
-  public Rook(){
+public class Rook{
+  public Rook(int c, int x, int y){
+  }
+  if (c==0){
+//then color is white
+  }
+  if (c==1){
+//then color is black
   }
 }
 
