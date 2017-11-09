7e57fd5eca1b3a4ecc72e444c73af39b4fa90e1d
NABabar <33323437+NABabar@users.noreply.github.com>
2017-11-08 19:56:12 -0500

    Update Coordinate.java

--- Really Good Chess/src/Coordinate.java
+++ Really Good Chess/src/Coordinate.java
@@ -1,2 +1,17 @@
 public class Coordinate{
+  String[] xarray = {"A", "B", "C", "D", "E", "F", "G", "H"};
+  String[] yarray = {"1", "2", "3", "4", "5", "6", "7", "8"};
+  int xcoordinate;
+  int ycoordinate;
+  public Coordinate(int x, int y){
+    xcoordinate = x;
+    ycoordinate = y;
+  }
+  public void Block(){
+    String Block = (String)(xarray[xcoordinate/100])+(String)(7-yarray[ycoordinate/100])
+  }
+  public void point(){
+    xcoordinate = ((xcoordinate/100)*100)+50;
+    ycoordinate = ((ycoordinate/100)*100)+50;
+  }
 }
